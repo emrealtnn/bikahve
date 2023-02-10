bf6a1d581ca13d0e2086acc726f19b383d3e752b
emrea <emrealtin1107@gmail.com>
2023-02-11 00:53:05 +0300

    giris

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDauLWiRQw4Bk1s7DnFhrVhZfWoThBhYFM",
+  authDomain: "bikahve-fa89e.firebaseapp.com",
+  projectId: "bikahve-fa89e",
+  storageBucket: "bikahve-fa89e.appspot.com",
+  messagingSenderId: "323099776179",
+  appId: "1:323099776179:web:5e1a6d7ba50a36a5e60cc6",
+  measurementId: "G-N441E7L6NH"
+};
